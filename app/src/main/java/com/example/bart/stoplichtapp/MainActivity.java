d54b57d2dbd076b3f9270cb3e1a072f253d8b8f7
bartjej <36732494+bartjej@users.noreply.github.com>
2018-06-06 15:12:47 +0200

    Fix van de lijst
    Fix de data

--- app/src/main/java/com/example/bart/stoplichtapp/MainActivity.java
+++ app/src/main/java/com/example/bart/stoplichtapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.bart.stoplichtapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.bart.stoplichtapp.List.CourseListActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void naarLijst(View v){
+        Intent naarlijst = new Intent(this, CourseListActivity.class);
+        startActivity(naarlijst);
     }
 }
