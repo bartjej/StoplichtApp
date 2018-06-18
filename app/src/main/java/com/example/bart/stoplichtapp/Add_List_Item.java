bde374890d09696e25bb420846a906109d45269e
bartjej <36732494+bartjej@users.noreply.github.com>
2018-06-18 16:00:53 +0200

    NEW evenement met tags nu
    werken alleen nog niet

--- app/src/main/java/com/example/bart/stoplichtapp/Add_List_Item.java
+++ app/src/main/java/com/example/bart/stoplichtapp/Add_List_Item.java
@@ -1,11 +1,13 @@
 package com.example.bart.stoplichtapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.bart.stoplichtapp.List.CourseListActivity;
 
 public class Add_List_Item extends AppCompatActivity {
 
@@ -14,6 +16,17 @@ public class Add_List_Item extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__list__item);
 
+        EditText naamedit = (EditText) findViewById(R.id.editName);
+        EditText infoedit = (EditText) findViewById(R.id.editInfo);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back = new Intent(Add_List_Item.this, CourseListActivity.class);
+                startActivity(back);
+            }
+        });
     }
 
 }
